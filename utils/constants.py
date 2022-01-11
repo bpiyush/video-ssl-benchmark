39e3ddc0b415f200a3e6926da98b9fb59bbacd25
Piyush Bagad <piyushnbagad11@gmail.com>
2022-01-11 14:17:26 +0100

    WIP: Video retrieval

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,8 @@
 """Defines constants"""
 
 
-UCF_DATA_PATH = '/ssd/fmthoker/ucf101/UCF-101'
-UCF_ANNO_PATH = '/ssd/fmthoker/ucf101/ucfTrainTestlist'
+# UCF_DATA_PATH = '/ssd/fmthoker/ucf101/UCF-101'
+# UCF_ANNO_PATH = '/ssd/fmthoker/ucf101/ucfTrainTestlist'
+
+UCF_DATA_PATH = '/local-ssd/fmthoker/ucf101/UCF-101'
+UCF_ANNO_PATH = '/local-ssd/fmthoker/ucf101/ucfTrainTestlist'
--- video_retrieval.py
+++ video_retrieval.py
@@ -3,12 +3,14 @@ Feature-based retrieval for a video.
 """
 
 from os.path import basename
+from collections import defaultdict
 import argparse
 import time
 from torch._C import EnumType
 import yaml
 import torch
 import wandb
+import numpy as np
 
 import utils.logger
 from utils import main_utils, eval_utils
@@ -98,7 +100,78 @@ def model_features_for_given_dataset(model, dataloader):
     return results
 
 
-def main_worker(gpu, ngpus, fold, args, cfg):
+def retrieval(
+    train_features,
+    train_labels,
+    train_vid_indices,
+    val_features,
+    val_labels,
+    val_vid_indices,
+    train_aud_features=None,
+    val_aud_features=None,
+    task='v-v',
+):
+    """
+    Computes retrieval scores for a given dataset.
+    """
+    from sklearn.neighbors import NearestNeighbors
+
+    assert task in ['v-a', 'a-v', 'v-v', 'a-a']
+    if task in ['v-a', 'a-v', 'a-a']:
+        assert(train_aud_features is not None)
+        assert(val_aud_features is not None)
+
+    if task == 'v-v':
+        feat_val = val_features
+        feat_train = train_features
+    elif task == 'v-a':
+        feat_val = val_features
+        feat_train = train_aud_features
+    elif task == 'a-v':
+        feat_val = val_aud_features
+        feat_train = train_features
+    elif task == 'a-a':
+        feat_val = val_aud_features
+        feat_train = train_aud_features
+
+    # Create 
+    neigh = NearestNeighbors(n_neighbors=50)
+    neigh.fit(feat_train)
+    recall_dict = defaultdict(list)
+    retrieval_dict = {}
+    for i in range(len(feat_val)):
+        feat = np.expand_dims(feat_val[i], 0)
+        vid_idx = val_vid_indices[i]
+        vid_label = val_labels[i]
+        retrieval_dict[vid_idx] = {
+            'label': vid_label,
+            'recal_acc': {
+                '1': 0, '5': 0, '10': 0, '20': 0, '50': 0
+            },
+            'neighbors': {
+                '1': [], '5':[], '10': [], '20': [], '50': []
+            }
+        }
+        for recall_treshold in [1, 5, 10, 20, 50]:
+            neighbors = neigh.kneighbors(feat, recall_treshold)
+            neighbor_indices = neighbors[1]
+            neighbor_indices = neighbor_indices.flatten()
+            neighbor_labels = set([train_labels[vid_index] for vid_index in neighbor_indices])
+            recall_value = 100 if vid_label in neighbor_labels else 0
+            acc_value = len([1 for neigh_label in neighbor_labels if neigh_label == vid_label]) / float(len(neighbor_labels))
+            retrieval_dict[vid_idx]['recal_acc'][str(recall_treshold)] = acc_value
+            retrieval_dict[vid_idx]['neighbors'][str(recall_treshold)] = neighbor_indices
+            recall_dict[recall_treshold].append(recall_value)
+        print(f'{i} / {len(feat_val)}', end='\r')
+
+    # Calculate mean recall values
+    for recall_treshold in [1, 5, 10, 20, 50]:
+        mean_recall = np.mean(recall_dict[recall_treshold]) 
+        print(f"{task}: Recall @ {recall_treshold}: {mean_recall}")
+    return retrieval_dict
+
+
+def main_worker(gpu, ngpus, fold, args, cfg, norm_feat=True):
     args.gpu = gpu
     args.world_size = ngpus
 
@@ -129,7 +202,29 @@ def main_worker(gpu, ngpus, fold, args, cfg):
 
     # get features
     train_results = model_features_for_given_dataset(model, train_loader)
-    debug()
+    test_results = model_features_for_given_dataset(model, test_loader)
+
+    # normalize features
+    if norm_feat:
+        train_results["features"] /= train_results["features"].norm(dim=1, p=2, keepdim=True)
+        test_results["features"] /= test_results["features"].norm(dim=1, p=2, keepdim=True)
+
+    # TODO: average features across clips of a video
+    # NOTE: currently, not doing averaging since I am using a single clip per video
+
+    # Get retrieval benchmarks
+    retrieval_dict = retrieval(
+        train_results["features"],
+        train_results["labels"],
+        list(range(len(train_results["features"]))),
+        test_results["features"],
+        test_results["labels"],
+        list(range(len(test_results["features"]))),
+        train_aud_features=None, 
+        val_aud_features=None, 
+        task='v-v',
+    )
+
 
 
 if __name__ == '__main__':
